c7f146197564686617f7af20f05820af58a13802
trailblazer <adarshraghuwanshi072@gmail.com>
2023-10-16 02:12:53 +0530

    done 16 oct

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 
-Shery.imageEffect("#back",{ style: 5});
-  
+Shery.imageEffect("#back", {
+    style: 5,
+    
+    config:
+    {
+        "a": { "value": 0.74, "range": [0, 30] },
+        "b": { "value": -0.98, "range": [-1, 1] },
+        "aspect": { "value": 2.0618893030104672 },
+        "gooey": { "value": true },
+        "infiniteGooey": { "value": true },
+        "durationOut": { "value": 1, "range": [0.1, 5] },
+        "durationIn": { "value": 1.5, "range": [0.1, 5] },
+        "displaceAmount": { "value": 0.5 }, "masker": { "value": true },
+        "maskVal": { "value": 1.05, "range": [1, 5] },
+        "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 },
+        "noEffectGooey": { "value": true }, "onMouse": { "value": 1 },
+        "noise_speed": { "value": 0.25, "range": [0, 10] },
+        "metaball": { "value": 0.15, "range": [0, 2] },
+        "discard_threshold": { "value": 0.31, "range": [0, 1] },
+        "antialias_threshold": { "value": 0.01, "range": [0, 0.1] },
+        "noise_height": { "value": 0.26, "range": [0, 2] },
+        "noise_scale": { "value": 14.88, "range": [0, 100] }
+    }, gooey: true
+});
+
