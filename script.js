21c81898ce64160e429659792b8f10b9cdbfe2c5
Adarsh Raghuwanshi <adarshraghuwanshi072@gmail.com>
2023-12-07 19:41:16 +0530

    final draft

--- script.js
+++ script.js
@@ -1,26 +1,64 @@
 
 Shery.imageEffect("#back", {
     style: 5,
-    
+    // debug:true,
     config:
-    {
-        "a": { "value": 0.74, "range": [0, 30] },
-        "b": { "value": -0.98, "range": [-1, 1] },
-        "aspect": { "value": 2.0618893030104672 },
-        "gooey": { "value": true },
-        "infiniteGooey": { "value": true },
-        "durationOut": { "value": 1, "range": [0.1, 5] },
-        "durationIn": { "value": 1.5, "range": [0.1, 5] },
-        "displaceAmount": { "value": 0.5 }, "masker": { "value": true },
-        "maskVal": { "value": 1.05, "range": [1, 5] },
-        "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 },
-        "noEffectGooey": { "value": true }, "onMouse": { "value": 1 },
-        "noise_speed": { "value": 0.25, "range": [0, 10] },
-        "metaball": { "value": 0.15, "range": [0, 2] },
-        "discard_threshold": { "value": 0.31, "range": [0, 1] },
-        "antialias_threshold": { "value": 0.01, "range": [0, 0.1] },
-        "noise_height": { "value": 0.26, "range": [0, 2] },
-        "noise_scale": { "value": 14.88, "range": [0, 100] }
-    }, gooey: true
+    {"a":{"value":0.92,"range":[0,30]},"b":{"value":-0.99,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":2.099453461897155},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.05,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":false},"onMouse":{"value":1},"noise_speed":{"value":0.31,"range":[0,10]},"metaball":{"value":0.1,"range":[0,2],"_gsap":{"id":3}},"discard_threshold":{"value":0.31,"range":[0,1]},"antialias_threshold":{"value":0.01,"range":[0,0.1]},"noise_height":{"value":0.26,"range":[0,2]},"noise_scale":{"value":14.88,"range":[0,100]}}, gooey: true
 });
+
+// for three elements
+// var elems = document.querySelectorAll(".elem");
+
+// elems.forEach(function () {
+
+//     var h1s = elem.querySelectorAll("h1");
+//     var index = 0;
+//     document.querySelector("#main")
+//         .addEventListener("click", function () {
+//             gsap.to(h1s[index], {
+//                 top: "-=100%",
+//                 ease: Expo.easeInOut,
+//                 duration: 1,
+//                 onComplete: function () {
+//                     gsap.set(this._targets[0], {
+//                         top: "100%"
+//                     })
+//                 }
+//             });
+
+//             index === h1s.length - 1 ? index = 0 : index++;
+//             gsap.to(h1s[index], {
+//                 top: 0,
+//                 ease: Expo.easeInOut,
+//                 duration: 1
+//             });
+
+
+//         })
+// })
+// for only one elem  **** don't forget to comment 2 lines in html
+var h1s = elem.querySelectorAll("h1");
+    var index = 0;
+    document.querySelector("#main")
+        .addEventListener("click", function () {
+            gsap.to(h1s[index], {
+                top: "-=100%",
+                ease: Expo.easeInOut,
+                duration: 1,
+                onComplete: function () {
+                    gsap.set(this._targets[0], {
+                        top: "100%"
+                    })
+                }
+            });
+
+            index === h1s.length - 1 ? index = 0 : index++;
+            gsap.to(h1s[index], {
+                top: 0,
+                ease: Expo.easeInOut,
+                duration: 1
+            });
+
+
+        })
 
