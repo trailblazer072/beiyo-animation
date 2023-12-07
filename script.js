e705578438a08ee0ecd52d4dbf22b43274370a2c
Adarsh Raghuwanshi <adarshraghuwanshi072@gmail.com>
2023-12-07 19:45:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,25 +39,29 @@ Shery.imageEffect("#back", {
 // for only one elem  **** don't forget to comment 2 lines in html
 var h1s = elem.querySelectorAll("h1");
     var index = 0;
-    document.querySelector("#main")
-        .addEventListener("click", function () {
-            gsap.to(h1s[index], {
-                top: "-=100%",
-                ease: Expo.easeInOut,
-                duration: 1,
-                onComplete: function () {
-                    gsap.set(this._targets[0], {
-                        top: "100%"
-                    })
-                }
-            });
-
-            index === h1s.length - 1 ? index = 0 : index++;
-            gsap.to(h1s[index], {
-                top: 0,
-                ease: Expo.easeInOut,
-                duration: 1
-            });
+    var animating =false;
+    document.querySelector("#main").addEventListener("click", function () {
+            if(!animating){
+                animating=true;
+                gsap.to(h1s[index], {
+                    top: "-=100%",
+                    ease: Expo.easeInOut,
+                    duration: 1,
+                    onComplete: function () {
+                        gsap.set(this._targets[0], {
+                            top: "100%"
+                        })
+                        animating=false;
+                    }
+                });
+    
+                index === h1s.length - 1 ? index = 0 : index++;
+                gsap.to(h1s[index], {
+                    top: 0,
+                    ease: Expo.easeInOut,
+                    duration: 1
+                });
+            }
 
 
         })
